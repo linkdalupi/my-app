dede31d8045e4e8ea9ab5d2878ca887635084fd5
linka <ilincnedel2710@student.lvusd.org>
2023-03-12 21:10:24 -0700

    only show recipe on submit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   let [apiResponse, setApiResponse] = useState(null)
+  let [submitted, setSubmitted] = useState(false)
   
   useEffect(() => {
     async function fetchData() {
@@ -16,15 +17,34 @@ function App() {
     }
     fetchData()
   })
-    
+  
+  function handleSubmit(event) {
+    setSubmitted(true)
+    event.preventDefault();  
+  }
+  
   return (
+    <form onSubmit={handleSubmit}>
     <div className="App">
       <header className="App-header">
+        <div>
+          <label> pizza dough</label>
+          <input type="checkbox" id="ingredient2" name="ingredient2" value="cheese"/>
+          <label> cheese</label>
+          <input type="checkbox" id="ingredient3" name="ingredient3" value="marinara sauce"/>
+          <label> marinara sauce</label>
+          <input type="checkbox" id="ingredient4" name="ingredient4" value="toppings"/>
+          <label> toppings</label>
+          <input type="checkbox" id="ingredient5" name="ingredient5" value="flour"/>
+          <label>flour</label>
+          <input type="submit" value="Submit"/>
+        </div>
         <p>
-          The recipe: {apiResponse}
+          The recipe: {submitted ? apiResponse: ""}
         </p>
       </header>
     </div>
+    </form>
   );
 }
 
